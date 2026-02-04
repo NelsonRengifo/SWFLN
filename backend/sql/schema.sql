8790db6daa04f6c0cce7863dfbe6ed0eeff672a2
NelsonRengifo <nrengifo3107@gmail.com>
2026-02-03 22:04:25 -0500

    added sessions table for tracking tokens

--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -1,6 +1,19 @@
 CREATE TABLE IF NOT EXISTS users (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     username TEXT UNIQUE NOT NULL,
     email TEXT UNIQUE NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    active BOOLEAN NOT NULL DEFAULT TRUE,
+    user_role TEXT NOT NULL DEFAULT 'admin',
     password_hash TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS sessions (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL,
+    token_hash TEXT NOT NULL UNIQUE,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    expires_at TIMESTAMPTZ NOT NULL DEFAULT (now() + INTERVAL '24 hours'),
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
