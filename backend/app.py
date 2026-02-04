629fddb977b0807c59161a87c95b8079a80a4013
NelsonRengifo <nrengifo3107@gmail.com>
2026-02-03 22:05:16 -0500

    loaded logging and .env variables

--- backend/app.py
+++ backend/app.py
@@ -2,22 +2,40 @@
 # EXTERNAL IMPORTS
 # ======================================================
 
+
 from fastapi import FastAPI
+from dotenv import load_dotenv
+from pathlib import Path
+
+
+# ======================================================
+# LOAD .env
+# ======================================================
+
+backend = Path(__file__).resolve().parent
+env_path = backend / ".env"
+load_dotenv(env_path, override=True)
+
 
 # ======================================================
 # INTERNAL IMPORTS
 # ======================================================
 
+
 from backend.routes.auth import auth_route
+from backend.core.logging_config import setup_logging
 
 # ======================================================
 # APP
 # ======================================================
 
+setup_logging()
 app = FastAPI()
+
 
 # ======================================================
 # ROUTE REGISTRATION
 # ======================================================
+
 
 app.include_router(auth_route)
