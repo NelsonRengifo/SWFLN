943ce20231b62fa653351cd9d79f4e0964b189bb
NelsonRengifo <nrengifo3107@gmail.com>
2026-03-07 11:07:28 -0500

    added new tables

--- backend/scripts/create_tables.py
+++ backend/scripts/create_tables.py
@@ -2,6 +2,7 @@
 # EXTERNAL IMPORTS
 # ======================================================
 
+
 from dotenv import load_dotenv
 from pathlib import Path
 
@@ -15,18 +16,33 @@ backend = Path(__file__).resolve().parent.parent
 env_path = backend / ".env"
 load_dotenv(env_path, override=True)
 
+
 # ======================================================
 # INTERNAL IMPORTS
 # ======================================================
 
 
+from backend.core.database_config import SessionLocal
 from backend import queries
 
 
 
 def create_tables() -> None:
-    queries.generate_schema()
 
+    db = SessionLocal()
+
+    try:
+        queries.generate_schema(db)
+        db.commit()
+        print("schema created")
+    
+    except Exception as e:
+        db.rollback()
+        print(f"failed to generate schema. ERROR: {e}")
+        raise
+    
+    finally:
+        db.close()
 
 if __name__ == "__main__":
     create_tables()
