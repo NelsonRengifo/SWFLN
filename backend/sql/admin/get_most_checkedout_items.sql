3d9cbff8d6b358367e15a65115868e8e635a079a
NelsonRengifo <nrengifo3107@gmail.com>
2026-03-27 15:28:21 -0400

    improved query so we can work with timestampz and not have to do casting to date

--- backend/sql/admin/get_most_checkedout_items.sql
+++ backend/sql/admin/get_most_checkedout_items.sql
@@ -5,9 +5,9 @@
 SELECT item_id, item_name, COUNT(*) as total
 FROM loans
 INNER JOIN uploaded_files ON loans.uploaded_file_id = uploaded_files.uploaded_file_id
-WHERE (:start_date IS NULL or uploaded_files.uploaded_at::DATE >= :start_date)
+WHERE (:start_date IS NULL OR uploaded_files.uploaded_at >= :start_date)
       AND
-      (:end_date IS NULL or uploaded_files.uploaded_at::DATE <= :end_date)
+      (:end_date IS NULL OR uploaded_files.uploaded_at < :end_date)
 GROUP BY item_name, item_id
 ORDER BY total DESC
-LIMIT 10;
+LIMIT :limit;
--- backend/sql/admin/get_top_organizations.sql
+++ backend/sql/admin/get_top_organizations.sql
@@ -2,8 +2,14 @@
  * Returns CursorResult object
  */
 
+-- < end_date to ensure we cover the entire last month due to timestamz column
+
 SELECT organization, COUNT(*) as borrowed_items
 FROM loans
+INNER JOIN uploaded_files ON loans.uploaded_file_id = uploaded_files.uploaded_file_id
+WHERE (:start_date IS NULL OR uploaded_files.uploaded_at >= :start_date)
+      AND
+      (:end_date IS NULL OR uploaded_files.uploaded_at < :end_date)
 GROUP BY organization
 ORDER BY borrowed_items DESC
 LIMIT :limit;
