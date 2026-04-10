df16c4f8054b062d66edf63dd97232af689fb205
NelsonRengifo <nrengifo3107@gmail.com>
2026-04-10 12:11:11 -0400

    fixed a bug where date range was not returning data

--- backend/sql/admin/get_most_checkedout_items.sql
+++ backend/sql/admin/get_most_checkedout_items.sql
@@ -2,12 +2,21 @@
  * Returns CursorResult object
  */
 
+-- SELECT item_id, item_name, COUNT(*) as total
+-- FROM loans
+-- INNER JOIN uploaded_files ON loans.uploaded_file_id = uploaded_files.uploaded_file_id
+-- WHERE (:start_date IS NULL OR uploaded_files.uploaded_at >= :start_date)
+--       AND
+--       (:end_date IS NULL OR uploaded_files.uploaded_at < :end_date)
+-- GROUP BY item_name, item_id
+-- ORDER BY total DESC
+-- LIMIT :limit;
+
 SELECT item_id, item_name, COUNT(*) as total
 FROM loans
-INNER JOIN uploaded_files ON loans.uploaded_file_id = uploaded_files.uploaded_file_id
-WHERE (:start_date IS NULL OR uploaded_files.uploaded_at >= :start_date)
+WHERE (:start_date IS NULL OR checkout_at >= :start_date)
       AND
-      (:end_date IS NULL OR uploaded_files.uploaded_at < :end_date)
+      (:end_date IS NULL OR checkout_at < :end_date)
 GROUP BY item_name, item_id
 ORDER BY total DESC
 LIMIT :limit;
